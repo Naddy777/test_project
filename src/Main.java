ad3de572d7f19ddb9beb845b956c6b6ae1d640fa
Naddy777 <ng-y@yandex.ru>
2025-07-08 16:57:11 +0300

    Вступление

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,9 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+    }
+class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
