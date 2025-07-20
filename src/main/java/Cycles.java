b9219aadb659789f56a7c16070cca4fe75579596
Naddy777 <ng-y@yandex.ru>
2025-07-21 01:34:20 +0300

    Циклы и условия

---
+++ src/main/java/Cycles.java
@@ -0,0 +1,35 @@
+package main.java;
+
+public class Cycles {
+    public static void main(String[] args) {
+        double a = Math.random();//функционал в джаве для вычисленний. рандом = значение будет от 0 до 1 (число с плавающей точкой)
+        double b = Math.random();
+
+        for(int i = 0; i < 10; i++){// первый вариант цикла
+            System.out.printf("%d", i);
+        }
+        int count = 0;
+        while (count < 10) {// второй вариант цикла, в скобках просто условие
+            System.out.printf("%d%n", count);
+            count++;//не забыть сделать переменную инкрементом, чтобы не сделать бесконечный цикл
+        }
+        if (a > b) {// Условия. Самая простая конструкция - только один выбор
+            System.out.println("Цикл 1: a > b, " + "a=" + a +"; b=" + b);
+        }
+        if (a * 100 < 50) {// два условия
+            System.out.println("Цикл 2, условие 1: a * 100 < 50, " + "a=" + a);
+        } else {
+            System.out.println("Цикл 2, условие 2: a * 100 >= 50, " + "a=" + a );
+        }
+        if (a * 100 < 10) {// много условий
+            System.out.println("Цикл 3, условие 1: a * 100 < 10, " + "a=" + a );
+        } else if (a * 100 < 20) {
+            System.out.println("Цикл 3, условие 2: a * 100 < 20, " + "a=" + a );
+        } else if (a * 100 < 30) {
+            System.out.println("Цикл 3, условие 3: a * 100 < 30, " + "a=" + a);
+        } else {
+            System.out.println("Цикл 3, условие 4: a * 100 >= 30, " + "a=" + a);
+        }
+
+    }
+}
