e8c26f1ba21d8dde3bdbf861166210bf5a318531
Naddy777 <ng-y@yandex.ru>
2025-07-27 23:19:02 +0300

    Циклы и условия

---
+++ src/main/java/testcode/Main.java
@@ -0,0 +1,33 @@
+package main.java.testcode;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+//Задача про обращение к пользователю в зависимости от времени суток
+public class Main {
+    public static void main(String[] args) {
+        int hour = LocalDateTime.now().getHour();
+        System.out.println(hour);//выделяем текущий час
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now().getDayOfMonth());// выделяем дату
+        System.out.println(LocalDateTime.now().getMonthValue());// выделяем номер месяца
+        System.out.println(LocalDateTime.now().getMonth());// выделяем название месяца
+        System.out.println(LocalDateTime.now().getChronology());// система времени, стандарт
+
+        Scanner scanner = new Scanner(System.in); // Создаем объект Сканер
+        System.out.print("Введите Ваше имя: ");
+        String name = scanner.nextLine(); //Считываем введенную строку. String - это строка, nextLine - считываем строку.
+        if (hour <= 12){
+            System.out.println("Доброе утро, " + name +"! Рад Вас видеть!");
+        }else if (hour <= 18){
+            System.out.println("Добрый день, " + name +"! Рад Вас видеть! Пора обедать!");
+        }else if (hour <= 22) {
+            System.out.println("Добрый вечер, " + name + "! Рад Вас видеть! ");
+        }else{
+            System.out.println("Доброй ночи, " + name +"! Рад Вас видеть! Но пора уже спать..");
+        }
+
+
+
+
+
+    }
+}
