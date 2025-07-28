f051dc865e7c8630f4e70e1ca3f7312a1398ae8c
Naddy777 <ng-y@yandex.ru>
2025-07-28 11:23:13 +0300

    Циклы и условия

--- src/main/java/testcode/Primer2.java
+++ src/main/java/testcode/Primer2.java
@@ -1,4 +1,26 @@
+/*Задача Напишите метод, который принимает на вход строку (String) и определяет, является ли строка палиндромом (возвращает булинговое значение)
+ */
 package main.java.testcode;
+//public static void main(String[] args) {
+//    System.out.println("Привет!");
+
+import java.util.Scanner;
 
 public class Primer2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Создаем объект Сканер
+        System.out.print("Введите слово или фразу: ");
+        String slovo = scanner.nextLine();
+        String revers = new StringBuilder(slovo)
+                .reverse().toString();
+        System.out.println(revers);
+        boolean palindrom=slovo.equalsIgnoreCase(revers);
+        if (palindrom){
+            System.out.printf("%s - палиндром.%n", slovo);
+        }else {
+            System.out.printf("%s - не палиндром.%n", slovo);
+        }
+
+
+    }
 }
