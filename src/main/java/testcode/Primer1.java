a3f2c97ae22efb63c70a9ed47720ba4a37d05f6d
Naddy777 <ng-y@yandex.ru>
2025-07-28 11:06:04 +0300

    Циклы и условия

---
+++ src/main/java/testcode/Primer1.java
@@ -0,0 +1,27 @@
+/*Задача: Дано четное число N>0 и символы с1 и с2.
+Написать метод, который вернет строку длины N, которая состоит из чередующихся символов С1 и С2 начиная с С1.
+ Для четных добавляем с1, для нечетных добавляем с2
+
+ */
+package main.java.testcode;
+
+public class Primer1 {
+    public static void main(String[] args) {
+        int N = 8;
+        char c1 = 'A';
+        char c2 = 'B';
+
+        StringBuilder sb = new StringBuilder(N);
+        for(int i=0; i<N; i++){
+            if(i%2==0){// %2 - это остаток от деления на 2 (не равен 0)
+                sb.append(c1);
+            }else{
+                sb.append(c2);
+            }
+            System.out.println(i);
+            System.out.println(sb);
+        }
+        String result=sb.toString();
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/testcode/Primer2.java
@@ -0,0 +1,4 @@
+package main.java.testcode;
+
+public class Primer2 {
+}
